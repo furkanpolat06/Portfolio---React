c60be607aa8e68ec2842725737ee2d2558e63f50
furkanpolat06 <emnplt.0698@gmail.com>
2023-11-09 21:39:43 +0300

    resume pages düzenleme

--- src/pages/experience/Resume.jsx
+++ src/pages/experience/Resume.jsx
@@ -1,29 +1,13 @@
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import React from 'react'
+import SectionHeader from './Section-header'
 
 const Resume = () => {
-  const styles = StyleSheet.create({
-    page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4'
-    },
-    section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
-    }
-  });
-  
   return (
-    <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
+    <div>
+
+<SectionHeader title="About Me" isButtonVisible={true} />
+     
+    </div>
   )
 }
 
---
+++ src/pages/experience/Section-header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaDownload } from "react-icons/fa";
+import "./section-header.css";
+import resumepng from "../../assets/img/Screenshot_3.png"
+
+const SectionHeader = () => {
+  return (
+    <div className="section-header">
+        <h2>BIO</h2>
+       <div className="header resumeLink">
+       <span className="text-shadow">
+        <a
+          href="https://docs.google.com/document/d/1FGBLDiSqJGf17fgJPPLviknZTrU49UBLu8GzfgYoh4M/edit"
+          className="resume-download "
+          data-toggle="tooltip"
+          data-placement="bottom"
+          title="Download"
+          target={"_blank"}
+        >
+          <i className="fa fa-download" aria-hidden="true">
+            {" "}
+          </i>{" "}
+         Download Resume
+        </a>
+       </span>
+       
+
+       </div>
+        <img className="m-auto" src={resumepng} alt="" />
+   
+   
+       
+      
+    </div>
+  );
+};
+
+export default SectionHeader;
